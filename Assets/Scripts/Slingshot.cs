007821fb5c0be015ffc097bc4c14c81da0f13241
JohnRene <31779197+Navias@users.noreply.github.com>
2018-10-03 13:45:54 -0400

    1/3 done
    up to 508

---
+++ Assets/Scripts/Slingshot.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour {
+
+    [Header("Set in Inspector")]
+    public GameObject prefabProjectile;
+    public float velocityMult = 8f;
+
+    [Header("Set Dynamically")]
+    public GameObject launchPoint;
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimingMode;
+
+    // this is giving a warning
+
+    private Rigidbody projectileRigidbody;
+
+
+    private void Awake()
+    {
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+
+        launchPos = launchPointTrans.position;
+    }
+
+    void OnMouseEnter()
+    {
+        //print(" Slingshot:OnMouseENter()");
+        launchPoint.SetActive(true);
+    }
+
+    void OnMouseExit()
+    {
+        //print("Slingshot:OnMouseExit()");
+        launchPoint.SetActive(false);
+
+    }
+
+    private void OnMouseDown()
+    {
+        aimingMode = true;
+
+        projectile = Instantiate(prefabProjectile) as GameObject;
+
+        projectile.transform.position = launchPos;
+
+        projectile.GetComponent<Rigidbody>().isKinematic = true;
+
+        projectileRigidbody = projectile.GetComponent<Rigidbody>();
+        projectileRigidbody.isKinematic = true;
+    }
+}
